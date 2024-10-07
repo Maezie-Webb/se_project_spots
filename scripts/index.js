c3815b62c7f1fba834c983ee281267534149e78d
maezie webb <webbmae3@gmail.com>
2024-10-07 13:31:09 -0700

    stage 4

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
   {
-    name: "An outdoor cafe",
+    name: "Restaurant Terrace",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
   },
   {
@@ -25,15 +25,56 @@ const initialCards = [
   },
 ];
 const profileEditButton = document.querySelector(".profile__edit-button");
-
 const editProfileModal = document.querySelector("#edit-profile-modal");
-
 const modalExitButton = document.querySelector(".modal__exit-btn");
+const ProfileName = document.querySelector(".profile__name");
+const editprofileForm = editProfileModal.querySelector(".modal__form");
+const ModalNameInput = editProfileModal.querySelector("#profile-name-input");
+const profileDescription = document.querySelector(".profile__description");
+const modalDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
-profileEditButton.addEventListener("click", function () {
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  console.log(data);
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const cardImgEl = cardElement.querySelector(".card__img");
+
+  cardNameEl.textContent = data.name;
+  cardImgEl.src = data.link;
+  cardImgEl.alt = data.name;
+  console.log(cardImgEl);
+  return cardElement;
+}
+
+function openModal() {
+  ModalNameInput.value = ProfileName.textContent;
+  modalDescriptionInput.value = profileDescription.textContent;
   editProfileModal.classList.add("modal__opened");
-});
+}
 
-modalExitButton.addEventListener("click", function () {
+function closeModal() {
   editProfileModal.classList.remove("modal__opened");
-});
+}
+function HandleEditFormsubmit(evt) {
+  evt.preventDefault();
+  ProfileName.textContent = ModalNameInput.value;
+  profileDescription.textContent = modalDescriptionInput.value;
+  closeModal();
+}
+
+profileEditButton.addEventListener("click", openModal);
+modalExitButton.addEventListener("click", closeModal);
+editprofileForm.addEventListener("submit", HandleEditFormsubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
